3f5c320b684e1e21ace42a4229f1107b2df69e74
Nelson <tomneo2004@gmail.com>
2022-03-03 19:24:49 +0800

    refactor: no change
    no change

--- src/pages/api/contact/index.ts
+++ src/pages/api/contact/index.ts
@@ -1,26 +1,26 @@
 import { NextApiRequest, NextApiResponse } from "next/types/index";
 import nextRequestHandler from "../../../utils/next/NextReqHandler";
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 import { MailOptions } from "nodemailer/lib/json-transport";
 
 const transporter = nodemailer.createTransport({
-    host: process.env.MAIL_SMTP,
-    port: Number(process.env.MAIL_SMTP_PORT) || 0,
-    auth: {
-        user: process.env.MAIL_SERVICE_USER,
-        pass: process.env.MAIL_SERVICE_PASS
-    }
-})
+  host: process.env.MAIL_SMTP,
+  port: Number(process.env.MAIL_SMTP_PORT) || 0,
+  auth: {
+    user: process.env.MAIL_SERVICE_USER,
+    pass: process.env.MAIL_SERVICE_PASS,
+  },
+});
 
 const SenderMissing = {
-    response:{
-        status: 400,
-        statusText: 'sender parameter is missing value',
-        data: {message:'sender parameter is missing value'},
-        headers: {},
-        config: {},
-    }
-}
+  response: {
+    status: 400,
+    statusText: "sender parameter is missing value",
+    data: { message: "sender parameter is missing value" },
+    headers: {},
+    config: {},
+  },
+};
 
 export default async function handler(
   req: NextApiRequest,
@@ -28,23 +28,19 @@ export default async function handler(
 ) {
   await nextRequestHandler(req, res, async (req) => {
     const successful = await transporter.verify();
-    if(successful) console.log('Ready to take mail message');
+    if (successful) console.log("Ready to take mail message");
 
-    const {
-        sender,
-        name,
-        content,
-    } = req.body;
+    const { sender, name, content } = req.body;
 
-    if(!sender) throw SenderMissing;
-    
-    const message:MailOptions = {
-        from: sender,
-        to: process.env.MAIL_RECIPIENT,
-        subject: `New contact from ${name}`,
-        text: content,
-        replyTo: sender
-    }
+    if (!sender) throw SenderMissing;
+
+    const message: MailOptions = {
+      from: sender,
+      to: process.env.MAIL_RECIPIENT,
+      subject: `New contact from ${name}`,
+      text: content,
+      replyTo: sender,
+    };
 
     const info = await transporter.sendMail(message);
     transporter.close();

cc41436bb19fe539fda85f4f774e4d28870d7eb5
Nelson <tomneo2004@gmail.com>
2022-03-07 16:07:30 +0800

    refactor: use sendgrid
    use sendgrid for email sending service

--- src/pages/api/contact/index.ts
+++ src/pages/api/contact/index.ts
@@ -1,17 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next/types/index";
 import nextRequestHandler from "../../../utils/next/NextReqHandler";
-// import nodemailer from "nodemailer";
-// import { MailOptions } from "nodemailer/lib/json-transport";
 import sendegrid from "@sendgrid/mail";
-
-// const transporter = nodemailer.createTransport({
-//   host: process.env.MAIL_SMTP,
-//   port: Number(process.env.MAIL_SMTP_PORT) || 0,
-//   auth: {
-//     user: process.env.MAIL_SERVICE_USER,
-//     pass: process.env.MAIL_SERVICE_PASS,
-//   },
-// });
 
 const SenderMissing = {
   response: {
@@ -30,25 +19,6 @@ export default async function handler(
   res: NextApiResponse
 ) {
   await nextRequestHandler(req, res, async (req) => {
-    // const successful = await transporter.verify();
-    // if (successful) console.log("Ready to take mail message");
-
-    // const { sender, name, content } = req.body;
-
-    // if (!sender) throw SenderMissing;
-
-    // const message: MailOptions = {
-    //   from: sender,
-    //   to: process.env.MAIL_RECIPIENT,
-    //   subject: `New contact from ${name}`,
-    //   text: content,
-    //   replyTo: sender,
-    // };
-
-    // const info = await transporter.sendMail(message);
-    // transporter.close();
-    // return info;
-
     const { sender, name, content } = req.body;
 
     if (!sender) throw SenderMissing;
